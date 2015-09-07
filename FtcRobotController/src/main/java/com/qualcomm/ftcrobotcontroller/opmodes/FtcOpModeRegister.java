a864b5c4394708513ad0508cf5b382d0fe1f20e9
Arjun Verma <arjunv0101@gmail.com>
2015-09-07 15:00:54 -0700

    test for gyro sensor

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -85,6 +85,8 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register("K9IrSeeker", K9IrSeeker.class);
     manager.register("K9Line", K9Line.class);
+    manager.register("TestGyroSensor", FTC7391.GyroSensorTest.class);
+
 
     /*
      * The following example op modes are designed to work with a pushbot-style robot.
