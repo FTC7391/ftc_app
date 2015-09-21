3ce0789edca636af62449b47b34054cfb1034d25
Arjun Verma <arjunv0101@gmail.com>
2015-09-20 17:07:13 -0700

    added empty shell for state machine

---
+++ FtcRobotController/src/main/java/FTC7391/stateMachine.java
@@ -0,0 +1,79 @@
+package FTC7391;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by ArjunVerma on 9/20/15.
+ */
+public class stateMachine extends OpMode {
+    private static DriveTrain driveTrain;
+
+
+    public void init(){
+
+    }
+
+    public void loop(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    private class State{
+        public State(){}
+
+        public void update(){
+
+        }
+
+        public State nextState(){
+            return this;
+        }
+    }
+
+    private class StartState extends State{
+        //override
+        public void update(){
+
+        }
+
+        public State nextState(){
+            return this;
+        }
+    }
+
+    private class ForwardState extends State{
+        //override
+        public void update(){
+
+        }
+
+        public State nextState(){
+            return this;
+        }
+    }
+
+    private class BackwardState extends State{
+        //override
+        public void update(){
+
+        }
+
+        public State nextState() {
+            return this;
+        }
+    }
+
+    private class DoneState extends State{
+        //override
+        public void update(){
+
+        }
+        
+    }
+
+
+
+}
