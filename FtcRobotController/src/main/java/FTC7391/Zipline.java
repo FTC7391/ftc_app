cb2e3d14f14fddfceb0ef2d29a3e0007a2bf5919
Reagan Haines <thereaganhaines@gmail.com>
2015-11-15 15:02:13 -0800

    Created a servo attachment base class and zipline class

---
+++ FtcRobotController/src/main/java/FTC7391/Zipline.java
@@ -0,0 +1,19 @@
+package FTC7391;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by JoshuaHaines on 11/15/15.
+ */
+public class Zipline extends ServoAttachment {
+
+//    private final double ARETRACTED_POS = 0;
+//    private final double DRIVE_POS = 10;
+//    private final double DEPLOYED_POS = 86;
+
+    public Zipline(HardwareMap hardwareMap) {
+        super(0, 10, 86);
+        servo = hardwareMap.servo.get("zipline");
+    }
+}
