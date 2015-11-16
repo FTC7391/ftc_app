90cfaa998ba5e7bc6f71b7aa8ab5db341ea20494
ishaanharry <ishaanharry@live.com>
2015-11-15 16:28:22 -0800

    cleaned code

--- FtcRobotController/src/main/java/FTC7391/ZiplineTest.java
+++ FtcRobotController/src/main/java/FTC7391/ZiplineTest.java
@@ -43,126 +43,39 @@ public class ZiplineTest extends OpMode {
     private Zipline zipline;
 
     private static final String TAG = Zipline.class.getSimpleName();
-    //private double power = 0;
 
-    /**
-     * Constructor
-     */
-    public ZiplineTest() {
-        zipline = new Zipline(hardwareMap);
-    }
-
-    /*
-     * Code to run when the op mode is initialized goes here
-     *
-     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#init()
-     */
     @Override
     public void init() {
         zipline = new Zipline(hardwareMap);
-        //DriveTrainAuto.moveInches(15, .5);
-		/*
-		 * Use the hardwareMap to get the dc motors and servos by name. Note
-		 * that the names of the devices must match the names used when you
-		 * configured your robot and created the configuration file.
-		 */
-		
-		/*
-		 * For the demo Tetrix K9 bot we assume the following,
-		 *   There are two motors "motor_1" and "motor_2"
-		 *   "motor_1" is on the right side of the bot.
-		 *   "motor_2" is on the left side of the bot and reversed.
-		 *   
-		 * We also assume that there are two servos "servo_1" and "servo_6"
-		 *    "servo_1" controls the arm joint of the manipulator.
-		 *    "servo_6" controls the claw joint of the manipulator.
-		 */
-
-        // assign the starting position of the wrist and claw
 
     }
 
-    /*
-     * This method will be called repeatedly in a loop
-     *
-     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
-     */
+
     @Override
     public void loop() {
 
         telemetry.addData(TAG, "OpMode Started");
 
-		/*
-		 * Gamepad 1
-		 * 
-		 * Gamepad 1 controls the motors via the left zipline, and it controls the
-		 * wrist/claw via the a,b, x, y buttons
-		 */
 
-        // throttle: left_zipline_y ranges from -1 to 1, where -1 is full up, and
-        // 1 is full down
-        // direction: left_zipline_x ranges from -1 to 1, where -1 is full left
-        // and 1 is full right
-		/*
-		float throttle = -gamepad1.left_zipline_y;
-		float direction = gamepad1.left_zipline_x;
-		float right = throttle - direction;
-		float left = throttle + direction;
-
-		// clip the right/left values so that the values never exceed +/- 1
-		right = Range.clip(right, -1, 1);
-		left = Range.clip(left, -1, 1);
-
-		// scale the joyzipline value to make it easier to control
-		// the robot more precisely at slower speeds.
-		right = (float)scaleInput(right);
-		left =  (float)scaleInput(left);
-		*/
-
-        //power = scaleInput(gamepad1.left_zipline_y);
 
         if (gamepad1.a) {
-            //DriveTrain.testRotateDegrees(positiveNumber);
+
             zipline.setRetractedPosition();
         }
         if (gamepad1.b) {
-            //DriveTrain.testRotateDegrees(negativeNumber);
+
             zipline.setDrivePosition();
         }
         if (gamepad1.y) {
-            //DriveTrain.testRotateDegrees(negativeNumber);
+
             zipline.setDeployedPosition();
         }
-//
-//        if (gamepad1.a) {
-//            zipline.setTestMode(zipline.TestModes.MODE_MOVE_ANGLE, power);
-//        }
-//        if (gamepad1.x) {
-//            zipline.setTestMode(zipline.TestModes.MODE_MOVE_BOTH, power);
-//        }
-
-
-        // clip the position values so that they never exceed their allowed range.
-        // armPosition = Range.clip(armPosition, ARM_MIN_RANGE, ARM_MAX_RANGE);
-        //clawPosition = Range.clip(clawPosition, CLAW_MIN_RANGE, CLAW_MAX_RANGE);
-
-		/*
-		 * Send telemetry data back to driver station. Note that if we are using
-		 * a legacy NXT-compatible motor controller, then the getPower() method
-		 * will return a null value. The legacy NXT-compatible motor controllers
-		 * are currently write only.
-		 */
 
     }
 
-    /*
-     * Code to run when the op mode is first disabled goes here
-     *
-     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
-     */
     @Override
     public void stop() {
-//stop
+
     }
 
 }
