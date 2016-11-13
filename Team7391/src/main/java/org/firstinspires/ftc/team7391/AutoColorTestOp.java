2d49158e782d40b40978733e56597be4445b411c
SocketPhys <SocketPhys@gmail.com>
2016-11-12 16:11:06 -0800

    ColorOp

---
+++ Team7391/src/main/java/org/firstinspires/ftc/team7391/AutoColorTestOp.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.team7391;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by Allana on 10/3/2015.
+ */
+@Autonomous(name = "Auto: LiftTest" , group = "Practice")
+public class AutoColorTestOp extends AutoOpBase
+{
+    private static final String TAG = AutoColorTestOp.class.getSimpleName();
+    private static final int INFINITE_WAIT = 0;
+
+    @Override
+    public void init()
+    {
+        super.init();
+        stepsList.add(new WaitState(0));//press a on gamepad 1
+        stepsList.add(new ClimbPositionState());
+        stepsList.add(new WaitState(0));//press a on gamepad 1
+        stepsList.add(new ReadyToHangPositionState());
+
+    }
+
+
+    @Override
+    public void stop()
+    {
+        telemetry.addData(TAG, "Test Stopped");
+        currentState = new StopState();
+    }
+
+}
